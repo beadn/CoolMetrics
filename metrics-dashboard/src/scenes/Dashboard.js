8893d7448cc88cf77e74e7fb722602619f2eeff8
beadn <beatrizdn@gmail.com>
2024-02-04 22:21:38 +0000

    Added Fetch to the server to get all metrics

--- metrics-dashboard/src/scenes/Dashboard.js
+++ metrics-dashboard/src/scenes/Dashboard.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { Box, Grid,  Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import axios from 'axios'; 
 
+const Dashboard = () => {
+  const [metricsData, setMetricsData] = useState([]);
 
-const Dashboard = ({ metricsData = [] }) => {
+  useEffect(() => {
+    const fetchMetrics = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/v1/metrics');
+        setMetricsData(Object.values(response.data));
+      } catch (error) {
+        console.error("Failed to fetch metrics:", error);
+
+      }
+    };
+
+    fetchMetrics();
+  }, []); 
+
   return (
     <Box>
       <Grid container spacing={2}>
         {metricsData.map((metricData, index) => (
           <Grid item xs={6} key={index}>
             <Box height="300px">
-              <Typography variant="h5" alignContent={"center"} textAlign={"center"}>
+              <Typography variant="h5" alignContent={"center"} textAlign={"center"} color="common.white">
                 {metricData.id}
               </Typography>
             </Box>
--- metrics-dashboard/src/scenes/Dashboard.test.js
+++ metrics-dashboard/src/scenes/Dashboard.test.js
@@ -1,17 +1,46 @@
-// src/components/Dashboard.test.js
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import axios from 'axios';
 import Dashboard from './Dashboard';
 
-describe('Dashboard Component', () => {
-  const mockMetricsData = [
-    { id: 'Metric1', value: 100 }, 
-    { id: 'Metric2', value: 200 }
-  ];
+// Mock axios
+jest.mock('axios');
 
-  test('renders metric ids', () => {
-    render(<Dashboard metricsData={mockMetricsData} />);
-    expect(screen.getByText('Metric1')).toBeInTheDocument();
-    expect(screen.getByText('Metric2')).toBeInTheDocument();
+describe('Dashboard', () => {
+  it('fetches metrics data and displays it', async () => {
+    // Mocking the axios.get method to return a successful response
+    axios.get.mockResolvedValue({
+      data: {
+        data: [
+          {
+            id: "510",
+            type: "metric",
+            attributes: {
+              name: "disk-usage",
+              timestamp: "2024-01-05T20:38:08.928Z",
+              value: "95.0"
+            }
+          },
+          {
+            id: "511",
+            type: "metric",
+            attributes: {
+              name: "memory-usage",
+              timestamp: "2024-01-05T20:38:08.928Z",
+              value: "22.0"
+            }
+          }
+        ]
+      }
+    });
+
+    const { findByText } = render(<Dashboard />);
+
+    // Wait for the axios data to be displayed
+    const firstMetricId = await findByText("510");
+    expect(firstMetricId).toBeInTheDocument();
+
+    const secondMetricId = await findByText("511");
+    expect(secondMetricId).toBeInTheDocument();
   });
 });
