fe57a2b2d9a00945fab08e2714aed0caed8968f3
beadn <beatrizdn@gmail.com>
2024-02-04 09:48:04 +0000

    Add index to metrics name

--- metrics-api/db/schema.rb
+++ metrics-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_03_005425) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_094434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_005425) do
     t.datetime "timestamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_metrics_on_name"
   end
 
 end
