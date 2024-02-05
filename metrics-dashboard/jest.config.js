1ea2cfaec4e075f8fab0a0923ec3a1004576f710
beadn <beatrizdn@gmail.com>
2024-02-05 00:25:54 +0000

    Set a theme to the UI

--- metrics-dashboard/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    transform: { '^.+\\.[t|j]sx?$': 'babel-jest',},
-    transformIgnorePatterns: ['<rootDir>/node_modules/(?!axios)'],
-  };
--- metrics-dashboard/src/scenes/Dashboard.js
+++ metrics-dashboard/src/scenes/Dashboard.js
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Grid, Typography } from '@mui/material';
-import axios from 'axios'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Grid, FormControl, Select, MenuItem, Typography, useTheme } from '@mui/material';
+import { tokens } from "../theme";
+import Header from "../components/Header";
+import { useCallback } from 'react';
 
 const Dashboard = () => {
+  
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [timeFrame, setTimeFrame] = useState('24h');
   const [metricsData, setMetricsData] = useState([]);
+  const fetchMetrics = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/metrics`, {
+        params: { time_frame: 'day'}});
+      setMetricsData(Object.values(response.data));
+    } catch (error) {
+      console.error("Failed to fetch metrics:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchMetrics = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/api/v1/metrics');
-        setMetricsData(Object.values(response.data));
-      } catch (error) {
-        console.error("Failed to fetch metrics:", error);
-
-      }
-    };
-
     fetchMetrics();
-  }, []); 
+  }, []);
 
   return (
-    <Box>
+    <Box m="20px"> 
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your fancy metrics" />
+      </Box>
+
       <Grid container spacing={2}>
         {metricsData.map((metricData, index) => (
           <Grid item xs={6} key={index}>
             <Box height="300px">
-              <Typography variant="h5" alignContent={"center"} textAlign={"center"} color="common.white">
+              <Typography variant="h5" color={colors.greenAccent[400]} alignContent={"center"} textAlign={"center"}>
                 {metricData.id}
               </Typography>
             </Box>
@@ -37,3 +47,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
